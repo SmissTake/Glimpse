9475ffdc14c0eb871aa7be87c3a2da9acc9744cf
Gwion Tangi <lil.smiss7@gmail.com>
2022-12-15 15:58:40 +0100

    Added Comment Model

---
+++ src/models/Comment.ts
@@ -0,0 +1,49 @@
+import {Model, DataTypes} from 'sequelize'
+import { sequelize } from '../config/database'
+
+import { User } from './User'
+import { Place } from './Place';
+
+export class Comment extends Model
+{
+    declare id: number;
+    declare comment: string;
+    declare usersId: number;
+    declare placesId: number;
+    declare created_at: Date;
+}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    usersId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        }
+    },
+    placesId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Place,
+            key: 'id',
+
+        }
+    },
+},
+{
+    sequelize,
+    tableName: "comments",
+    createdAt: "created_at"
+}
+);
