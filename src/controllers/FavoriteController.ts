9843b10fbfc57b56bf9e7a372007778cc587a9b0
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-12 14:47:28 +0200

    added token validation to favorite

--- src/controllers/FavoriteController.ts
+++ src/controllers/FavoriteController.ts
@@ -1,26 +1,44 @@
 import { Request, Response } from "express";
 import { CrudController } from "./CrudController";
 import { Favorite } from "../models/Favorite";
+import { validateToken } from "../authenticate/jwt";
 
 export class FavoriteController extends CrudController {
     public create(req: Request, res: Response): void {
-        if (!req.body.usersId) {
-            res.status(400).json({ error: "Missing usersId in request body" });
-        }
-
-        Favorite.create(req.body)
-        .then((favorite) => res.json(favorite))
-        .catch((err) => res.status(500).json(err));
+        const token = req.headers.authorization?.split(' ')[1];
+        validateToken(token!)
+        .then(decoded => {
+            const usersId = decoded.usersId;
+            const favorite = req.body;
+            favorite.usersId = usersId;
+            Favorite.create(favorite)
+            .then((favorite) => res.json(favorite))
+            .catch((err) => res.status(500).json(err));
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error liking place"});
+        });
     }
 
     public delete(req: Request, res: Response): void {
-        Favorite.destroy({
-            where: {
-                usersId: req.body.usersId,
-                placesId: req.body.placesId
-            }
+        const token = req.headers.authorization?.split(' ')[1];
+        validateToken(token!)
+        .then(decoded => {
+            const usersId = decoded.usersId;
+            const placesId = req.body.placesId;
+            Favorite.destroy({
+                where: {
+                    usersId: usersId,
+                    placesId: placesId
+                }
+            })
+            .then((favorite) => res.json(favorite))
+            .catch((err) => res.status(500).json(err));
         })
-        .then((favorite) => res.json(favorite))
-        .catch((err) => res.status(500).json(err));
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: "Error unliking place"});
+        });
     }
 }
