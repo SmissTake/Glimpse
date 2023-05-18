2eb02543fe1236a52c2fbba4475ba535d36c38bb
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-19 01:42:25 +0200

    added test place/listall & place/show/:id

--- __tests__/routes/place.test.ts
+++ __tests__/routes/place.test.ts
@@ -4,6 +4,16 @@ import app from '../../src/index';
 
 
 /**
+ * Testing the GET /place/show/:id route
+ */
+describe('GET /place/show/:id', () => {
+  it('should return 200 OK', async () => {
+    const response = await request(app).get('/place/show/1');
+    expect(response.status).toBe(200);
+  });
+});
+
+/**
  * Testing the GET /place/listall route
  */
 describe('GET /place/listall', () => {
@@ -11,33 +21,28 @@ describe('GET /place/listall', () => {
     const response = await request(app).get('/place/listall');
     expect(response.status).toBe(200);
   });
-});
 
-// describe('POST /api/my-endpoint', () => {
-//   it('should return 200 OK', async () => {
-//     const res = await request(app)
-//       .post('/api/my-endpoint')
-//       .field('title', 'ENCORE2')
-//       .field('description', 'Hopital abandonné depuis une quarantaine d\'année, beaucoup d\'équipement ancien')
-//       .field('history', 'L\'hôpital abandonné est un bâtiment imposant et effrayant, construit dans les années 1920. Il a été fermé dans les années 80 en raison de la modernisation des soins médicaux. Les fenêtres sont brisées, la peinture s\'écaille et l\'herbe pousse sur le terrain autour de l\'édifice. Les patients et le personnel ont été déplacés vers d\'autres hôpitaux, mais les lits, les instruments médicaux et les documents administratifs restent encore à l\'intérieur. Les rumeurs disent que l\'hôpital est hanté par les fantômes des patients décédés. Il est dangereux de pénétrer à l\'intérieur en raison de l\'état de délabrement avancé du bâtiment.')
-//       .field('town', 'Douarnenez')
-//       .field('keyword', 'hopital, medical, batiment')
-//       .field('categoriesId', '1')
-//       .field('accessibilitiesId', '1')
-//       .attach('pictures', 'path/to/picture1.jpg')
-//       .attach('pictures', 'path/to/picture2.jpg')
-//       .attach('pictures', 'path/to/picture3.jpg');
-    
-//     expect(res.status).toBe(200);
-//   });
-// });
+  it('should return JSON', async () => {
+    const response = await request(app).get('/place/listall');
+    expect(response.type).toBe('application/json');
+  });
 
-/**
- * Testing the GET /place/show/:id route
- */
-describe('GET /place/show/:id', () => {
-  it('should return 200 OK', async () => {
-    const response = await request(app).get('/place/show/1');
-    expect(response.status).toBe(200);
+  it('should return an array of places', async () => {
+    const response = await request(app).get('/place/listall');
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('should return places with id, title, town, created_at, pictures, user id and pseudonym, category label, accessibility label, and favorite users', async () => {
+    const response = await request(app).get('/place/listall');
+    const place = response.body[0];
+    expect(place).toHaveProperty('id');
+    expect(place).toHaveProperty('title');
+    expect(place).toHaveProperty('town');
+    expect(place).toHaveProperty('created_at');
+    expect(place).toHaveProperty('PicturePlaces');
+    expect(place).toHaveProperty('postedBy');
+    expect(place).toHaveProperty('Category');
+    expect(place).toHaveProperty('Accessibility');
+    expect(place).toHaveProperty('FavoriteUsers');
   });
 });
