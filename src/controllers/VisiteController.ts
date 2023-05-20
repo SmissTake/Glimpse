d77647f7b922727841fb48f1bf98d06bd8faee6c
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-21 00:09:08 +0200

    added token /visit

--- src/controllers/VisiteController.ts
+++ src/controllers/VisiteController.ts
@@ -1,16 +1,24 @@
 import { Request, Response } from "express";
 import { CrudController } from "./CrudController";
 import { Visite } from "../models/Visite";
+import { validateToken } from "../authenticate/jwt";
 
 export class VisiteController extends CrudController {
     public create(req: Request, res: Response): void {
-        if (!req.body.usersId) {
-            res.status(400).json({ error: "Missing usersId in request body" });
-        }
-
-        Visite.create(req.body)
-        .then((visite) => res.json(visite))
-        .catch((err) => res.status(500).json(err));
+        const token = req.headers.authorization?.split(' ')[1];
+        validateToken(token!)
+        .then(decoded => {
+            const usersId = decoded.usersId;
+            const visite = req.body;
+            visite.usersId = usersId;
+            Visite.create(visite)
+            .then((visite) => res.json(visite))
+            .catch((err) => res.status(500).json(err));
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error liking place"});
+        });
     }
 
     public delete(req: Request, res: Response): void {
