f687c1c0e283bdfcc9d64b0b7b01afc72bf18db5
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-21 22:54:33 +0200

    added auth token to follow

--- src/controllers/FollowController.ts
+++ src/controllers/FollowController.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from "express";
 import { CrudController } from "./CrudController";
 import { Follow } from "../models/Follow";
+import { validateToken } from "../authenticate/jwt";
 
 export class FollowController extends CrudController {
     public create(req: Request, res: Response): void {
-        if (!req.body.followerId) {
-            res.status(400).json({ error: "Missing followerId in request body" });
-        }
-
-        Follow.create(req.body)
-        .then((follow) => res.json(follow))
-        .catch((err) => res.status(500).json(err));
+        const token = req.headers.authorization?.split(' ')[1];
+        validateToken(token!)
+        .then(decoded => {
+            const followerId = decoded.usersId;
+            if (!req.body.followingId) {
+                res.status(400).json({ error: "Missing followingId in request body" });
+            }
+            else {
+                const follow = {
+                    followingId : req.body.followingId,
+                    followerId : followerId
+                };
+                Follow.create(follow)
+                .then((follow) => res.json(follow))
+                .catch((err) => res.status(500).json(err));
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "Error following user"});
+        });
     }
 
     public delete(req: Request, res: Response): void {
